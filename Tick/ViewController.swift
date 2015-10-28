a7d8d11cdc753905193959e6c9cfaa233c72f47a
Martin Schinz <m.schinz@gmail.com>
2015-10-28 15:51:57 +0000

    Initial Commit

---
+++ Tick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tick
+//
+//  Created by Martin Schinz on 28/10/2015.
+//  Copyright © 2015 pebble{code}. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
